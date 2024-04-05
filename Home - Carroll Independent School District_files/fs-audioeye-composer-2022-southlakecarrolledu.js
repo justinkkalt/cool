a9b019fa9e7fc2c183a53b61cff5fb29dc44fb3d
justinkkalt <166079285+justinkkalt@users.noreply.github.com>
2024-04-04 19:18:18 -0500

    Add files via upload

---
+++ Home - Carroll Independent School District_files/fs-audioeye-composer-2022-southlakecarrolledu.js
@@ -0,0 +1,7 @@
+if(document.body.className.indexOf('fsDraftMode') === -1){
+
+!function(){var b=function(){window.__AudioEyeSiteHash = "537168e3b9471c5742a962859064a7f9"; var a=document.createElement("script");a.src="https://wsmcdn.audioeye.com/aem.js";a.type="text/javascript";a.setAttribute("async","");document.getElementsByTagName("body")[0].appendChild(a)};"complete"!==document.readyState?window.addEventListener?window.addEventListener("load",b):window.attachEvent&&window.attachEvent("onload",b):b()}();
+
+document.getElementById("fsPageContent").setAttribute("style", "outline:none;");
+
+}
